765bf744f59f416000e53a7af74e86f98cfaed74
Clown <is511723@gmail.com>
2026-05-13 23:46:50 +1000

    feat(about): Создает секцию "Как работает Veltrix?" на странице "О нас"

--- frontend/app/about/page.tsx
+++ frontend/app/about/page.tsx
@@ -1,10 +1,12 @@
 import Hero from "@/components/sections/about/Hero";
 import Reasons from "@/components/sections/about/Reasons";
+import Works from "@/components/sections/about/Works";
 export default function HomePage() {
     return (
         <main>
             <Hero/>
             <Reasons/>
+            <Works/>
         </main>
     );
 }
---
+++ frontend/components/sections/about/Works/Works.tsx
@@ -0,0 +1,49 @@
+import Container from "@/components/layout/Container";
+import style from './Works.module.scss';
+import WorkType from "@/components/sections/about/Works/WorkType";
+
+const works: WorkType[] = [
+    {
+        text: 'Выбираете категорию',
+    },
+    {
+        text: 'Настраиваете параметры',
+    },
+    {
+        text: 'Сравниваете предложения',
+    },
+    {
+        text: 'Выбираете лучшее',
+    },
+
+]
+
+export default function Works() {
+    return (
+        <div className={style.works}>
+            <Container>
+                <div className={style.works__content}>
+                    <h2 className={style.works__title}>
+                        Как работает Veltrix?
+                    </h2>
+                    <div className={style.works__cards}>
+                        {works.map((item, index) => {
+                            return (
+                                <div className={`${index % 2 === 0 ? style.works__left : style.works__right}`} key={index}>
+                                    <div className={style.works__card}>
+                                        <h4 className={style.works__number}>
+                                            {index+1}
+                                        </h4>
+                                        <p className={style.works__text}>
+                                            {item.text}
+                                        </p>
+                                    </div>
+                                </div>
+                            )
+                        })}
+                    </div>
+                </div>
+            </Container>
+        </div>
+    )
+}
