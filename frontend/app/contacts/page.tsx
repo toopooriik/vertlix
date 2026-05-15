1782a9ba3af843693c8bec7d0ba6294c42e02475
Clown <is511723@gmail.com>
2026-05-15 22:32:18 +1000

    feat(contacts): Создает страницу контактов

---
+++ frontend/app/contacts/page.tsx
@@ -0,0 +1,10 @@
+
+import Contacts from "@/components/sections/contacts";
+
+export default function HomePage(){
+    return(
+        <main>
+            <Contacts/>
+        </main>
+    )
+}
---
+++ frontend/components/sections/contacts/Contacts.tsx
@@ -0,0 +1,38 @@
+
+import Container from "@/components/layout/Container";
+import style from './Contacts.module.scss';
+export default function Contacts(){
+    return(
+        <div className={style.contacts}>
+            <Container>
+                <div className={style.contacts__content}>
+                    <div className={style.contacts__left}>
+                        <h2 className={style.contacts__title}>Контакты</h2>
+                        <div className={style.contacts__author}>
+                            <p className={style.contacts__author_name}>Шелюта Дмитрий Игоревич</p>
+                            <p className={style.contacts__author_email}>is511723@gmail.com</p>
+                            <p className={style.contacts__author_phone}>+7(914)428-47-34</p>
+                        </div>
+                    </div>
+                    <div className={style.contacts__right}>
+                        <form action="" className={style.contacts__form}>
+                            <div className={`${style.contacts__full_name} ${style.contacts__form_input}`}>
+                                <label htmlFor={'full-name'}><span>ФИО</span></label>
+                                <input type="text" id={'full-name'} placeholder={'Иванов Иван Иванович'}/>
+                            </div>
+                            <div className={`${style.contacts__email} ${style.contacts__form_input}`}>
+                                <label htmlFor={'email'}><span>Почта</span></label>
+                                <input type="text" id={'email'} placeholder={'example@gmail.com'}/>
+                            </div>
+                            <div className={`${style.contacts__comment} ${style.contacts__form_input}`}>
+                                <label htmlFor={'comment'}><span>Комментарий</span></label>
+                                <input type="text" id={'comment'} placeholder={'Комментарий'}/>
+                            </div>
+                            <button type={'submit'} className={style.contacts__submit}>Отправить</button>
+                        </form>
+                    </div>
+                </div>
+            </Container>
+        </div>
+    )
+}
